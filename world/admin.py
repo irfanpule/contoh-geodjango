a9e9856c65c3131e64ce91c06d38d1a9515d8b67
irfanpule <irfan.pule2@gmail.com>
2020-04-17 20:33:20 +0700

    admin world modify

--- world/admin.py
+++ world/admin.py
@@ -1,5 +1,10 @@
 from django.contrib.gis import admin
 from .models import WorldBorder
 
-# admin.site.register(WorldBorder, admin.GeoModelAdmin)
-admin.site.register(WorldBorder, admin.OSMGeoAdmin)
+
+class WorldGeoAdmin(admin.OSMGeoAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+admin.site.register(WorldBorder, WorldGeoAdmin)
